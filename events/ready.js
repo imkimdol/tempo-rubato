dbc410e6c970f793835e03c0c4439128e28ab989
lodmik <kimdol@proton.me>
2023-07-17 08:20:27 -0700

    Add custom status/activity

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,7 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        if (process.env.STATUS) client.user.setStatus(process.env.STATUS);
+        if (process.env.ACTIVITY_TYPE && process.env.ACTIVITY_MESSAGE) client.user.setActivity(process.env.ACTIVITY_MESSAGE, { type: parseInt(process.env.ACTIVITY_TYPE) });
     },
 };
