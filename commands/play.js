4a227dcdcbc0f652639f10b1ccb80d1a7c7f75c9
lodmik <kimdol@proton.me>
2023-09-10 19:31:09 -0700

    Add multi query support for play command

--- commands/play.js
+++ commands/play.js
@@ -3,7 +3,10 @@ const { useMainPlayer } = require("discord-player");
 const { getAverageColor } = require('fast-average-color-node');
 
 const { checkChannelType, checkInVoice } = require('../helpers/check');
-const { editReply, handleError } = require('../helpers/message');
+const { editReply, handleError, addRows } = require('../helpers/message');
+
+
+const checkNoResult = (result) => typeof result === 'string' && result.equals('ERR_NO_RESULT');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,9 +15,9 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('search')
-                .setDescription('Play using a search query.')
+                .setDescription('Play using a search query. Separate by commas for multi-query.')
                 .addStringOption(option =>
-                    option.setName('search').setDescription('Enter search query.').setRequired(true)
+                    option.setName('query').setDescription('Enter search query.').setRequired(true)
                 ))
         .addSubcommand(subcommand =>
             subcommand
@@ -39,48 +42,69 @@ module.exports = {
             if (!checkInVoice(interaction, client)) return;
             if (!client.playRates[interaction.guild.id]) client.playRates[interaction.guild.id] = 1;
             
-            let search;
+            let input;
             if (interaction.options.getSubcommand() === 'favourites') {
                 const bank = interaction.options.getInteger('bank');
                 try {
-                    search = await client.db.get([interaction.user.id, bank]);
+                    input = await client.db.get([interaction.user.id, bank]);
                 } catch (err) {
                     console.error(err);
                     return editReply(`Failed to retrieve data from bank ${bank}.`, interaction, client);
                 }
             } else {
-                search = interaction.options.getString('search');
+                input = interaction.options.getString('query');
             }
             
+            searches = input.split(/,\s*/);
             const player = useMainPlayer();
-            let playResult;
-            try {
-                playResult = await player.play(
-                    interaction.member.voice.channel,
-                    search,
-                    {
-                        requestedBy: interaction.user,
-                        nodeOptions: {
-                            metadata: interaction.channel
-                        }
-                    },
-                );
-            } catch (err) {
-                console.error(err);
-                if (err.name === 'ERR_NO_RESULT') {
-                    return editReply(`Found no results!`, interaction, client);
+            const playResults = [];
+            for (let search of searches) {
+                try {
+                    const playResult = await player.play(
+                        interaction.member.voice.channel,
+                        search,
+                        {
+                            requestedBy: interaction.user,
+                            nodeOptions: {
+                                metadata: interaction.channel,
+                                skipOnNoStream: false
+                            }
+                        },
+                    );
+                    playResults.push(playResult);
+
+                } catch (err) {
+                    console.error(err);
+
+                    if (err.name === 'ERR_NO_RESULT') {
+                        playResults.push('ERR_NO_RESULT');
+                    } else {
+                        throw err;
+                    }
                 }
-                throw err;
-            }
+            }    
 
             const embed = new EmbedBuilder();
             const userImage = `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png`;
             const userColour = await getAverageColor(userImage);
-    
+
             embed.setTitle('Added to Queue')
                 .setColor(userColour.hex)
-                .addFields({ name: playResult.track.title, value: playResult.track.author })
-                .setFooter({ text: `Queue Size: ${playResult.queue.size}` });
+                .setFooter({ text: `Queue Size: ${playResults[0].queue.size}` });
+            
+            if (playResults.length === 1) {
+                result = playResults[0];
+                if (checkNoResult(result)) return editReply(`Found no results!`, interaction, client);
+                embed.addFields({ name: result.track.title, value: result.track.author });
+            } else if (playResults.length > 1) {
+                let tracks = [];
+                playResults.map((r) => {
+                    if (!checkNoResult(r)) tracks = tracks.concat(r.track);
+                });
+                embed.addFields(...addRows(tracks, 12));
+            } else {
+                return editReply(`Failed to add songs.`, interaction, client);
+            }
 
             editReply({ embeds: [embed] }, interaction, client);
 
