36f66b379ad4b08e6a015bc0c150ee0a4d4f48f8
imkimdol <kimdol@proton.me>
2024-03-26 22:08:35 -0700

    Fix "constant" variables

--- src/controller/DatabaseController.ts
+++ src/controller/DatabaseController.ts
@@ -4,14 +4,14 @@ export enum DatabaseBanks { Bank1, Bank2, Bank3, Bank4, Bank5 }
 
 // Singleton
 export class DatabaseController {
-    private readonly location = 'levelDB';
-    private readonly options: DatabaseOptions<string, string> = { keyEncoding: 'json' };
+    private static readonly location = 'levelDB';
+    private static readonly options: DatabaseOptions<string, string> = { keyEncoding: 'json' };
 
     private static instance: DatabaseController;
     private database: Level;
 
     private constructor() {
-        this.database = new Level(this.location, this.options);
+        this.database = new Level(DatabaseController.location, DatabaseController.options);
     }
 
     static getInstance(): DatabaseController {
