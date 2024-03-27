24ed176c456a7b4fd3c6d44484072576e3d95a45
imkimdol <kimdol@proton.me>
2024-03-26 21:45:00 -0700

    Add LevelDB initialization

--- src/controller/DatabaseController.ts
+++ src/controller/DatabaseController.ts
@@ -1,18 +1,22 @@
-import { Level } from "level";
+import { DatabaseOptions, Level } from "level";
 
 export enum DatabaseBanks { Bank1, Bank2, Bank3, Bank4, Bank5 }
 
 // Singleton
 export class DatabaseController {
+    private readonly location = 'levelDB';
+    private readonly options: DatabaseOptions<string, string> = { keyEncoding: 'json' };
+
     private static instance: DatabaseController;
     private database: Level;
 
     private constructor() {
-        throw new Error('Not Implemented.');
+        this.database = new Level(this.location, this.options);
     }
 
     static getInstance(): DatabaseController {
-        throw new Error('Not Implemented.');
+        if (!this.instance) this.instance = new DatabaseController();
+        return this.instance;
     };
 
     setBank(query: string, userId: string, bank: DatabaseBanks) {
